139e4356fa5f47bae342669715346ca7062225b3
MCR300400 <edoardopippi00@gmail.com>
2026-09-15 23:06:26 +0200

    feat(db): register ep-algorithms in siti table

---
+++ migrations/0003_aggiungi_algorithms.sql
@@ -0,0 +1,3 @@
+INSERT INTO siti (id, nome, dominio, chiave, creato_il) VALUES
+  ('algorithms', 'ep-algorithms', 'ep-algorithms.pages.dev, algorithms.edoardopippi.dev, edoardopippi.dev',
+   'ep_algorithms_7a8f9c2d1b4e', unixepoch() * 1000);
